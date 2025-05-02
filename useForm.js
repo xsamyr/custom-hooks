639cdcb3778a4a1fd07a0cc6224fce0535ccf082
Samyr <samyrjauregui@gmail.com>
2025-05-01 19:32:27 -0500

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+
+export const useForm = (initialForm = {}) => {
+
+    const [formState, setFormState] = useState(initialForm)
+
+    const onInputChanged = ({ target }) => {
+
+        const { name, value } = target;
+
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    }
+
+    const onResetForm=()=>{
+         setFormState(initialForm);
+    }
+
+    return {
+        ...formState,
+        formState,
+        onInputChanged,
+        onResetForm
+    }
+
+}
